0ed2877ff89051bfeb90d15126741f449a26ee04
Dvojak <Walter.obona@gmail.com>
2025-09-09 18:31:30 +0200

    It_didnt_add?

--- player.gd
+++ player.gd
@@ -1,1 +1,19 @@
 extends Node2D
+var units = []
+
+func _ready():
+	units = [$Unit1, $Unit2, $Unit3]
+
+func reset_units():
+	for u in units:
+		u.reset_activation()
+
+func has_units_to_activate() -> bool:
+	for u in units:
+		if not u.activated:
+			return true
+	return false
+
+func start_turn():
+	# Tady můžeš dát hráči možnost vybrat jednotku
+	print("Vyber jednotku k aktivaci.")
--- unit.gd
+++ unit.gd
@@ -1,1 +1,13 @@
-extends Node2D
+
+extends Node
+
+var activated = false
+
+func reset_activation():
+	activated = false
+
+func activate():
+	if activated:
+		return
+	print("Jednotka ", name, " je aktivována!")
+	activated = true
